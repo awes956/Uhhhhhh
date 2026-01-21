be3697a0c4dbb5acbd33dc8a95c2afdc9e1037c8
Neko Collector <nekocollector@terminal.com>
2026-01-21 13:03:47 +0800

    :3

--- source/reanim.lua
+++ source/reanim.lua
@@ -4797,7 +4797,7 @@ function HatReanimator.Start()
 						elseif v.Name == "Neck" then
 							Util.SetMotor6DOffset(v, torsooffset.Rotation:Inverse() * CFrame.new(math.random() * 0.05, 1.5, -20))
 						else
-							Util.SetMotor6DOffset(v, torsooffset.Rotation:Inverse() * CFrame.new(i * -3, math.random() * 0.05, -0.2))
+							Util.SetMotor6DOffset(v, torsooffset.Rotation:Inverse() * CFrame.new(i * -3, math.random() * 0.05, -3))
 							i += 1
 						end
 					end
